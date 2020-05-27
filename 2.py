5b94d7b86f8c43128357afd79ed11ee5fcacb5b3
danmuruk <66041890+danmuruk@users.noreply.github.com>
2020-05-28 08:23:22 +0900

    Add files via upload

--- 2.py
+++ 2.py
@@ -17,4 +17,4 @@ async def on_message(message):
         await message.channel.send("현재 삼뚝이는 명령어를 지원하지 않습니다. 이후 버전에 업데이트 될 예정입니다.")
 
 
-client.run("NzE1MzM1NjM0NzA4ODU2ODgz.Xs7uaA.thbh15XwsN0Ko2zZZ0XQ4GVk-tg")
+client.run("NzE1MzM1NjM0NzA4ODU2ODgz.Xs72Qg.nf8zl2C8UHVUjULmbvtMREHANsk")
